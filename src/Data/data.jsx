0c22b459cab945d96eed108bd15813f2c742d4d7
aryan710 <92682930+aryan710@users.noreply.github.com>
2023-12-29 22:57:36 +0530

    Login and Register page

---
+++ src/Data/data.jsx
@@ -0,0 +1,33 @@
+export const SidebarMenu = [
+    {
+        name :"Home",
+        path:'/',
+        icon :'fa-solid fa-house',
+    },
+    {
+        name:'Appointments',
+        path:"/appoinments",
+        icon:'fa-solid fa-list',
+
+    },
+    {
+        name: 'Apply Doctor',
+        path: '/apply-doctor',
+        icon: '<i class="fa-solid fa-user"></i>',
+    },
+    {
+        name: 'Profile',
+        path: '/profile',
+        icon: 'fa-solid fa-user',
+    },
+    {
+        name: 'Logout',
+        path: '/logout',
+        icon: 'fa-solid fa-arrow-right-from-bracket',
+    },
+
+
+
+
+
+]
